8454d60c848fee0a0c584deef25964bee2c2b38e
aketc <prisaketch@gmail.com>
2021-10-29 13:03:40 +0300

    eventweb

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "../components/Layout";
+
+export default function AboutPage() {
+    return (
+        <Layout title="about page">
+            <h1>About Page</h1>
+            <p>this is a page about my events</p>
+        </Layout>
+    );
+}
+
+
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "Next/link"
+import Layout from "../components/Layout";
+
+export default function HomePage() {
+  return (
+    <Layout title="Events Page">
+      <h1>Home Page</h1>
+    </Layout>
+  );
+}
